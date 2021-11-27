1d362e2b5943b66f958754dcd07d501b99721293
amin <aminrezaei@gmail.com>
2021-11-27 20:38:07 +0330

    try inline

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const product = require("./api/product");
+// const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
-
-app.use("/api/product", product);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+// app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
